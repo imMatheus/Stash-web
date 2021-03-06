fb72ac5ce01aee7990d4b5abbcf93d7480bf4290
Matheus <77362975+imMatheus@users.noreply.github.com>
2021-03-06 12:14:06 +0100

    init 2

--- stash/src/App.js
+++ stash/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { Counter } from './features/counter/Counter'
+import Navbar from './components/navbar/Navbar'
+
+import Sidebar from './components/sidebar/Sidebar'
+import './global.css'
 
 function App() {
-    return <div className='App'>hejjejje</div>
+    return (
+        <div className='App'>
+            <Sidebar />
+        </div>
+    )
 }
 
 export default App
---
+++ stash/src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <div className='navbar-logo'>Stash</div>
+        </div>
+    )
+}
+
+export default Navbar
