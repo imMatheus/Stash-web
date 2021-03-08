8d3aee473f45540059933ae968e4b5225cc93946
Matheus <77362975+imMatheus@users.noreply.github.com>
2021-03-08 21:30:32 +0100

    starting base markup for the layout

--- stash/src/components/stashesOverview/stashesOverview-components/StOv-storage/StashStorage.jsx
+++ stash/src/components/stashesOverview/stashesOverview-components/StOv-storage/StashStorage.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import StorageItem from './StorageItem'
 
 function StashStorage() {
+    const [loading, setLoading] = useState(true)
+    const [fetchedData, setFetchedData] = useState(null)
+    useEffect(() => {
+        const dummyFunction = async () => {
+            await fetch('https://fakestoreapi.com/products?limit=20')
+                .then((res) => res.json())
+                .then((json) => setFetchedData(json))
+            console.log(fetchedData)
+            setLoading(false)
+        }
+
+        dummyFunction()
+    }, [])
+
     let testImage =
         'https://images.unsplash.com/photo-1597106776019-b4ecc878c202?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
     return (
         <div className='stashstorage'>
             <div className='storage-sorting'></div>
             <div className='storage-itemsContainer'>
-                <StorageItem itemImage={testImage} />
-                <StorageItem itemImage={testImage} />
-                <StorageItem itemImage={testImage} />
-                <StorageItem itemImage={testImage} />
-                <StorageItem itemImage={testImage} />
-                <StorageItem itemImage={testImage} />
-                <StorageItem itemImage={testImage} />
-                <StorageItem itemImage={testImage} />
+                <StorageItem />
+                <StorageItem itemImage={testImage} itemName='vatten flaska' />
+                {loading
+                    ? null
+                    : fetchedData.map((item) => {
+                          return (
+                              <StorageItem
+                                  key={item.id}
+                                  itemImage={item.image}
+                                  itemName={item.title}
+                                  itemPrice={item.price}
+                                  itemsInStore={item.id}
+                              />
+                          )
+                      })}
             </div>
         </div>
     )
--- stash/src/components/stashesOverview/stashesOverview-components/StOv-storage/StorageItem.jsx
+++ stash/src/components/stashesOverview/stashesOverview-components/StOv-storage/StorageItem.jsx
@@ -1,13 +1,42 @@
 import React from 'react'
+import background from '../../../../images/stash-men.jpg'
 
-function StorageItem({ itemImage }) {
-    let selectedImage = itemImage
+function StorageItem({ itemImage, itemName, itemsInStore, itemPrice }) {
+    let testId = itemImage + ''
     return (
-        <div class='storageitem'>
-            <div
-                className='storageitem-image'
-                style={{ backgroundImage: `url(${selectedImage})` }}
-            ></div>
+        <div className='storageitem' load='lazy'>
+            <input type='checkbox' id={testId} />
+            <label htmlFor={testId}></label>
+            <div className='storageitem-overhall' for='testing'>
+                <div
+                    className='storageitem-image'
+                    style={{ backgroundImage: `url(${itemImage || background})` }}
+                ></div>
+                <div className='storageitem-info'>
+                    <div>
+                        <h6>Product Name:</h6>
+                        <h3>{itemName || 'No Name Given'}</h3>
+                    </div>
+                    <div>
+                        <h6>Items In Store:</h6>
+
+                        <p>{itemsInStore || '0'}</p>
+                    </div>
+                    <div>
+                        <h6>Product Price:</h6>
+
+                        <p>${itemPrice || '0'}</p>
+                    </div>
+                </div>
+            </div>
+            {/* a drop down menu for a more detailed status of the product */}
+            <div className='storageitem-dropdown'>
+                <div
+                    className='storageitem-dropdown-image'
+                    style={{ backgroundImage: `url(${itemImage || background})` }}
+                ></div>
+                <div className='storageitem-dropdown-info'>hej</div>
+            </div>
         </div>
     )
 }
